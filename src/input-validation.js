a24004f4fd19c4e8564171a14669bb3bdf2fc291
hishamazad <hishamazad@gmail.com>
2020-11-14 09:38:29 +0530

    Update input-validation.js

--- src/input-validation.js
+++ src/input-validation.js
@@ -3,22 +3,11 @@ const Apify = require('apify');
 const { log } = Apify.utils;
 
 // Small hack for backward compatibillity
-// Previously there was a checkbox includeImages and includeReviews. It had to be on.
-// maxImages and maxReviews 0 or empty scraped all
 // Right now, it works like you woudl expect, 0 or empty means no images, for all images just set 99999
-// If includeReviews/includeImages is not present, we process regularly
 module.exports.makeInputBackwardsCompatible = (input) => {
     // Deprecated on 2020-07
-    if (input.includeReviews !== undefined || input.includeImages !== undefined) {
-        log.warning('INPUT DEPRECATION: includeReviews and includeImages input fields have been deprecated and will be removed soon! Use maxImage and maxReviews instead');
-    }
-    if (input.includeReviews === true && !input.maxReviews) {
-
-        input.maxReviews = 999999;
-    }
-
-    if (input.includeReviews === false) {
-        input.maxReviews = 0;
+    if (input.includeImages !== undefined) {
+        log.warning('INPUT DEPRECATION: includeImages input fields have been deprecated and will be removed soon! Use maxImage instead');
     }
 
     if (input.includeImages === true && !input.maxImages) {
